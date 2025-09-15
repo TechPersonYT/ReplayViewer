4e67501f683454a9bb11f1c86ece32b66ee950d5
TechPersonYT <39715756+TechPersonYT@users.noreply.github.com>
2025-09-15 12:57:15 -0500

    Interpolate frames

--- src/main.zig
+++ src/main.zig
@@ -3,6 +3,7 @@ const rl = @import("raylib");
 const rp = @import("replay.zig");
 
 const FORWARD: rl.Vector3 = .{.x = 0.0, .y = 0.0, .z = 1.0};
+const ONE: rl.Vector3 = .{.x = 1.0, .y = 1.0, .z = 1.0};
 
 const WebReplayInfo = struct {
     replay_url: []u8,
@@ -115,6 +116,48 @@ fn downloadMusic(url: []u8, gpa: std.mem.Allocator) !rl.Music {
     return sound;
 }
 
+const TransformInfo = struct {
+    position: rl.Vector3,
+    rotation: rl.Quaternion,
+    rotation_matrix: rl.Matrix,
+    rotation_axis: rl.Vector3,
+    rotation_angle: f64,
+    transform: rl.Matrix,
+    direction: rl.Vector3
+};
+
+fn computeAllForms(position: rl.Vector3, rotation: rl.Quaternion) TransformInfo {
+    const rotation_matrix = rl.Quaternion.toMatrix(rotation);
+
+    var rotation_axis: rl.Vector3 = undefined;
+    var rotation_angle: f32 = 0.0;
+    rl.Quaternion.toAxisAngle(rotation, &rotation_axis, &rotation_angle);
+
+    const transform = rl.Matrix.multiply(rotation_matrix, rl.Matrix.translate(position.x, position.y, position.z));
+
+    const direction = FORWARD.transform(rotation_matrix);
+
+    return .{.position = position, .rotation = rotation, .rotation_matrix = rotation_matrix, .rotation_axis = rotation_axis, .rotation_angle = rotation_angle, .transform = transform, .direction = direction};
+}
+
+fn interpolateFrames(a: *const rp.ReplayFrame, b: *const rp.ReplayFrame, time: f64) rp.ReplayFrame {
+    const t = rl.math.remap(@floatCast(time), a.time, b.time, 0.0, 1.0);
+
+    return .{
+        .time = rl.math.lerp(a.time, b.time, t),
+        .fps = b.fps,
+
+        .head_position = rl.Vector3.lerp(a.head_position, b.head_position, t),
+        .head_rotation = rl.Quaternion.lerp(a.head_rotation, b.head_rotation, t),
+
+        .left_hand_position = rl.Vector3.lerp(a.left_hand_position, b.left_hand_position, t),
+        .left_hand_rotation = rl.Quaternion.lerp(a.left_hand_rotation, b.left_hand_rotation, t),
+
+        .right_hand_position = rl.Vector3.lerp(a.right_hand_position, b.right_hand_position, t),
+        .right_hand_rotation = rl.Quaternion.lerp(a.right_hand_rotation, b.right_hand_rotation, t),
+    };
+}
+
 pub fn main() !void {
     // Initialization
     const screen_width = 1280;
@@ -176,12 +219,16 @@ pub fn main() !void {
     var left_saber_material = try rl.loadMaterialDefault();
     var right_saber_material = try rl.loadMaterialDefault();
 
-    // albedo, emission, roughness
+    // TODO: emission, roughness
     rl.setMaterialTexture(&head_material, .albedo, head_texture);
     rl.setMaterialTexture(&left_saber_material, .albedo, left_saber_texture);
     rl.setMaterialTexture(&right_saber_material, .albedo, right_saber_texture);
 
     rl.playMusicStream(music);
+
+    var music_time: f64 = rl.getMusicTimePlayed(music);
+    var replay_time: f64 = music_time;
+    var last_music_sync: f64 = rl.getTime();
 
     // Main game loop
     while (!rl.windowShouldClose()) { // Detect window close button or ESC key
@@ -215,35 +262,43 @@ pub fn main() !void {
             camera.begin();
             defer camera.end();
 
+            // Sync with music every second
+            if (rl.getTime() - last_music_sync > 1.0) {
+                music_time = rl.getMusicTimePlayed(music);
+                last_music_sync = rl.getTime();
+            }
+
+            replay_time = music_time + rl.getTime() - last_music_sync;
+
+            // Sync frame with replay time
+            while (@as(f64, @floatCast(replay.frames.items(.time)[frame_index])) < replay_time) {
+                frame_index += 1;
+            }
+
+            if (frame_index > 0) {
+                frame_index -= 1;
+            }
+
+            const interpolated_frame = interpolateFrames(&replay.frames.get(frame_index), &replay.frames.get(frame_index + 1), replay_time);
+
             // Head
-            var position = replay.frames.items(.head_position)[frame_index];
-            var rotation = rl.Quaternion.toMatrix(replay.frames.items(.head_rotation)[frame_index]);
-            var transform = rl.Matrix.multiply(rotation, rl.Matrix.translate(position.x, position.y, position.z));
-            var ray: rl.Ray = .{.position = position, .direction = FORWARD.transform(transform)};
-            rl.drawMesh(head_mesh, head_material, transform);
+            var transform_info = computeAllForms(interpolated_frame.head_position, interpolated_frame.head_rotation);
+            rl.drawMesh(head_mesh, head_material, transform_info.transform);
 
             // Left hand
-            position = replay.frames.items(.left_hand_position)[frame_index];
-            rotation = rl.Quaternion.toMatrix(replay.frames.items(.left_hand_rotation)[frame_index]);
-            transform = rl.Matrix.multiply(rotation, rl.Matrix.translate(position.x, position.y, position.z));
-            rl.drawMesh(saber_hilt_mesh, left_saber_material, transform);
-            ray = .{.position = position, .direction = FORWARD.transform(rotation)};
-            rl.drawLine3D(position, position.add(ray.direction.scale(2)), .red);
+            transform_info = computeAllForms(interpolated_frame.left_hand_position, interpolated_frame.left_hand_rotation);
+            rl.drawMesh(saber_hilt_mesh, left_saber_material, transform_info.transform);
+            rl.drawLine3D(transform_info.position, transform_info.position.add(transform_info.direction.scale(2)), .red);
 
             // Right hand
-            position = replay.frames.items(.right_hand_position)[frame_index];
-            rotation = rl.Quaternion.toMatrix(replay.frames.items(.right_hand_rotation)[frame_index]);
-            transform = rl.Matrix.multiply(rotation, rl.Matrix.translate(position.x, position.y, position.z));
-            rl.drawMesh(saber_hilt_mesh, right_saber_material, transform);
-            ray = .{.position = position, .direction = FORWARD.transform(rotation)};
-            rl.drawLine3D(position, position.add(ray.direction.scale(2)), .blue);
+            transform_info = computeAllForms(interpolated_frame.right_hand_position, interpolated_frame.right_hand_rotation);
+            rl.drawMesh(saber_hilt_mesh, right_saber_material, transform_info.transform);
+            rl.drawLine3D(transform_info.position, transform_info.position.add(transform_info.direction.scale(2)), .blue);
 
             rl.drawGrid(10, 0.5);
         }
 
-        while (@as(f64, @floatCast(replay.frames.items(.time)[frame_index])) < rl.getMusicTimePlayed(music)) {
-            frame_index += 1;
-        }
+        rl.drawFPS(0, 0);
     }
 }
 
